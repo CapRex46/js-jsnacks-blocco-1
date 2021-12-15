0137f741ba49b60bdc09f2f6f5bf9073914071f0
agres <ale.agresta46@gmail.com>
2021-12-15 15:34:48 +0100

    first

---
+++ snack1/script.js
@@ -0,0 +1,18 @@
+const numbers =[
+firstnum = prompt('digita il numero'),
+secondnum = prompt('digita il numero'),
+thirdnum = prompt('digita il numero'),
+fourthnum = prompt('digita il numero'),
+fifthnum = prompt('digita il numero'),
+sixthnum = prompt('digita il numero'),
+seventhnum = prompt('digita il numero'),
+eightiethnum = prompt('digita il numero'),
+ninethnum = prompt('digita il numero'),
+tenthnum = prompt('digita il numero'),
+]
+
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++){
+    console.log(sum += numbers, [i] [1])
+}
